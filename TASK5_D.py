d669bf66dee8884dc3583b3d01a50cdc090902df
Agamjot158 <106953882+Agamjot158@users.noreply.github.com>
2022-11-01 12:17:28 +0530

    Update TASK5_D.py

--- TASK5_D.py
+++ TASK5_D.py
@@ -1,4 +1,6 @@
-import time
+# Libraries used for the task -
+
+import time          
 from time import sleep
 from tkinter import *
 import tkinter.font
@@ -6,15 +8,15 @@ from gpiozero import LED
 import RPi.GPIO
 from tkinter import messagebox
 
-RPi.GPIO.setmode(RPi.GPIO.BOARD)   
+RPi.GPIO.setmode(RPi.GPIO.BOARD)   # using the GPIO Board setup for the task
 
-led = LED(14)    
+led = LED(14)    # I am using GPIO pin 14, i-e physical pin 8
 
-win = Tk()    
+win = Tk()    # creating a gui titled "Morse Code"
 win.title("Morse Code")    
-Font = tkinter.font.Font(family = 'Cambria' , size = 14, weight = "bold")
+Font = tkinter.font.Font(family = 'Cambria' , size = 14, weight = "bold") # Here I have selected the font and size 
 
-morse_code = {
+morse_code = {      # Here I have defined every character in morse code 
       'A': '.-', 
       'B': '-...', 
       'C': '-.-.', 
@@ -43,32 +45,32 @@ morse_code = {
       'Z': '--..', 
 }
 
-def dot():
+def dot():   # I have defined the dot function for my code-
     led.on()
     time.sleep(0.02)
     led.off
     
-def dash():
+def dash():  # I have defined the dash function for my code -
     led.on()
     time.sleep(1.8)
     led.off()
     
-def gap():
+def gap():  # Here I have created a gap function for creating a space between 2 letters
     led.off()
     time.sleep(2)
 
-def morseToString(Name):
+def morseToString(Name): # Here I have created a function that takes the input and compares it with our letter dictionary  
     morse_string = ""
-    if len(Name) > 0 and len(Name) <= 12:
+    if len(Name) > 0 and len(Name) <= 12: # here I fixed the lenght of the string input
         for c in Name:
             morse_string += morse_code[c.upper()]
             morse_string += " "
         return morse_string
     else:
-        messagebox.showerror("Error", "Name cannot exceed 12 characters or null")
+        messagebox.showerror("Error", "Name cannot exceed 12 characters or null") # Then the input string doesn't fits the fixed lenght this message would be displayed
         return ""
 
-def Sentence():
+def Sentence(): # This function fetches the converted string and compares it with the letter dictionary and then prints the sentence in morse code
     led.off()
     senText = Name.get()
     conversion = morseToString(senText)
@@ -87,9 +89,11 @@ def Sentence():
 
 led.off()
 
-def close():
+def close(): # Exit command
     led.off()
     win.destroy()
+
+ # Creations of the buttons - 
 
 Name = Entry(win,font=Font, width=30) 
 Name.grid(row=0, column=1)
